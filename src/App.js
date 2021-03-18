ec66618fbbe199c2c90d0a9fc7e8cb5169787222
ddawkins <ddawkins7@gmail.com>
2021-03-18 18:20:38 -0400

    updated sentry/ store dev and prod -3

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ import ErrorBoundary from './components/error/ErrorBoundary';
 import { appRoutes } from './redux/actions/routes';
 
 const App = (props) => {
-
   console.log(process.env);
 
   const { appRoutes } = props;
