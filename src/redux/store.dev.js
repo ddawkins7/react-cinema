d2074c4b8beb7db9e0b6d9098578e3a9127831f1
ddawkins <ddawkins7@gmail.com>
2021-03-18 21:31:38 -0400

    updated development code

--- src/redux/store.dev.js
+++ src/redux/store.dev.js
@@ -1,12 +1,17 @@
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
+import { createLogger } from 'redux-logger';
 import * as Sentry from '@sentry/react';
 
 import rootReducers from './reducers';
 
+const reduxLogging = createLogger({
+  collapsed: true,
+});
+
 const initialState = {};
-const middleware = [thunk];
+const middleware = [require('redux-immutable-state-invariant').default(), thunk, reduxLogging];
 const sentryReduxEnhancer = Sentry.createReduxEnhancer({
   // Optionally pass options
 });
